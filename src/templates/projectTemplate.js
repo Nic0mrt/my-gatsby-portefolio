c5ef9daf451f31961c4b7faea10dabac420c7e8c
Nicolas MARET <maret.nicolas@gmail.com>
2020-10-05 19:49:07 +0200

    add boxshadow in project template and add margin bottom

--- src/templates/projectTemplate.js
+++ src/templates/projectTemplate.js
@@ -17,7 +17,14 @@ export default function Template({
     <Layout>
       <div className="project-section">
         <h3>{frontmatter.title} </h3>
-        <Img fluid={featuredImage} alt={alt}></Img>
+        <Img
+          style={{
+            boxShadow: "0px 0px 10px 3px rgba(0, 0, 0, 0.22)",
+            marginBottom: "30px",
+          }}
+          fluid={featuredImage}
+          alt={alt}
+        ></Img>
         <Link to={githubLink} alt={alt} target="_blank" rel="noreferrer">
           Voir le code source sur Github
         </Link>
